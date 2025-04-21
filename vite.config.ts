898a2dfda5ec23247987ee86b7a106a5aa54eeee
ricardoaf7 <ricardo.af7@gmail.com>
2025-04-20 23:38:22 -0300

    claude 13 . git pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/kichute-2025/", // Nome do seu repositório
   server: {
     host: "::",
     port: 8080,
